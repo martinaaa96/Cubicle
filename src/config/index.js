9452c2a0c3c7e8d08c37270dc7f2eb8ee2e69fd5
Martina Vangelova <mvangelovaa@abv.bg>
2023-02-03 17:00:37 +0200

    add mongoose
    add cube model

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 const config = {
     production: {
-        PORT: 1234
+        PORT: 1234,
+        DB_URI: 'mongodb://127.0.0.1:27017/cubbicle'
     },
     development: {
         PORT: 5000,
+        DB_URI: 'mongodb://127.0.0.1:27017/cubbicle'
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const routes = require('./routs')
+
+const routes = require('./routs');
 const config = require('./config');
-const setUpViewEngine= require('./config/viewEngine')
+const setUpViewEngine= require('./config/viewEngine');
+const initDatabase = require('./config/dbInit');
+
+
 
 const app = express();
 
@@ -11,6 +15,8 @@ setUpViewEngine(app);
 app.use(express.static('src/public'))
 
 app.use(express.urlencoded({extended: false}));
-app.use(routes)
+app.use(routes) 
 
-app.listen(config.PORT, ()=> console.log(`Server is running on port ${config.PORT}...`))
+initDatabase()
+.then(()=>app.listen(config.PORT, ()=> console.log(`Server is running on port ${config.PORT}...`)))
+.catch((err)=> console.log(err.message) )
