426836595b1b90c2c9c363d98974c75d2d94af60
Martina Vangelova <mvangelovaa@abv.bg>
2023-02-05 14:18:10 +0200

    fixed paths

--- src/routs.js
+++ src/routs.js
@@ -13,23 +13,17 @@ router.get('/', homeController.getHomePage)
 router.get('/about', homeController.getAboutPage)
 router.get("/404",homeController.getErrorPage)
 
-
-
 //app.get('/create', (req,res)=>{
     ///res.render('create')
 //})
 
-
-
-
-
-router.get('/create', cubeController.getCreateCube);
-router.post('/create', cubeController.postCreateCube)
+router.get('/cubes/create', cubeController.getCreateCube);
+router.post('/cubes/create', cubeController.postCreateCube)
 router.get('/cubes/:cubeId/details', cubeController.getDetails);
 
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory)
 router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
 
-router.use('/accessory',accessoryController);
+router.use('/accessories',accessoryController);
 
 module.exports = router;
