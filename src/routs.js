7f1cfa7edf892886f1d2f04468c88914b4bb258b
Martina Vangelova <mvangelovaa@abv.bg>
2023-02-11 14:54:05 +0200

    add login and register form

--- src/routs.js
+++ src/routs.js
@@ -7,11 +7,13 @@ const cubeController = require('./controllers/cubeController')
 
 const homeController = require('./controllers/homeController')
 const accessoryController = require('./controllers/accessoryController')
-
+const authController = require('./controllers/authController')
 
 router.get('/', homeController.getHomePage)
 router.get('/about', homeController.getAboutPage)
 router.get("/404",homeController.getErrorPage)
+
+router.use('/',authController)
 
 //app.get('/create', (req,res)=>{
     ///res.render('create')
