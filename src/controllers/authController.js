e6256081abc8e0522777f5d0d48a4ace0f24509e
Martina Vangelova <mvangelovaa@abv.bg>
2023-02-15 11:04:38 +0200

    validations

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ router.post('/login', async (req, res) => {
 
     } catch (err) {
        
-console.log(err)
+console.log(err.message)
       
     }
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const config = require('../config')
 const jwt = require('../lib/jsonwebtoken')
+const AppError = require('../utils/AppError')
 
 exports.getUserByUserName = (username) => User.findOne({ username });
 
@@ -9,18 +10,28 @@ exports.register = (username, password) => User.create({ username, password });
 
 exports.login = async (username, password) => {
 
-    const user = await this.getUserByUserName(username)
+  const user = await this.getUserByUserName(username)
 
-    const isValid = await user.validatePassword(password);
+  const isValid = await user.validatePassword(password);
 
-    if (!user || !isValid) {
+  if (!user) {
+    throw new AppError('Invalid username', { user })
 
-        throw 'Invalid user name or password';
-    }
 
-  const payload = {_id: user._id,  username: user.username }
-  const token = await jwt.sign(payload, config.SECRET, { expiresIn: '2h'});
+    //throw {
+    // message:'Invalid username',
+    //data: user 
+    // }
+  }
+  if (!user || !isValid) {
+    throw new AppError('Invalid password');
+    //throw {
+      //message: 'Invalid  password'
+    //}
+  }
+  const payload = { _id: user._id, username: user.username }
+  const token = await jwt.sign(payload, config.SECRET, { expiresIn: '2h' });
 
-    return token;
+  return token;
 
 }
